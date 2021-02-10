94636be83623169c162cdbc64ce5635a4d9d1c69
Manasi Keskar <ManasiKeskar@users.noreply.github.com>
2021-02-10 15:22:27 -0800

    Adding error boundaries to the component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,13 @@ import React from 'react';
 import classes from './Person.module.css';
 
 const Person = ( props ) => {
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
+    
     return (
-        // <div className="Person" style={style}>
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
