bd6e2ef313b1da71bac75d220afd111a46323ffb
Manasi Keskar <ManasiKeskar@users.noreply.github.com>
2021-02-12 14:10:42 -0800

    passing props in chain

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -18,10 +18,9 @@ class Person extends Component {
     }
     render() {
         console.log('[Person.js] rendering...')
-        console.log("Person Name = ", this.props.name);
         return (
-            
             <Aux>
+                {this.props.isAuth ? <p>Authenticated</p> : <p>Please Login</p>}
                 <p key='i1' onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old.
                 </p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -82,6 +83,10 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
+  loginHnadler = () => {
+    this.setState({authenticated: true});
+  }
+
   render() {
     console.log("[App.js] render");
     let persons = null;
@@ -92,6 +97,7 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
     }
 
@@ -110,6 +116,7 @@ class App extends Component {
               showPersons={this.state.showPersons}
               personsLength={this.state.persons.length}
               clicked={this.togglePersonHandler}
+              login={this.loginHnadler}
             />) : null
           }
           {persons}
