d9ba13344793303a587b1cd7b81b3253b2c058fc
Manasi Keskar <ManasiKeskar@users.noreply.github.com>
2021-02-11 17:18:02 -0800

    React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,19 +6,19 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Aux>
-                {/* <div className={classes.Person}> */}
-                    <p onClick={this.props.click}>
-                        I'm {this.props.name} and I am {this.props.age} years old.
-                    </p>
-                    <p>{this.props.children}</p>
-                    <input 
-                        type='text' 
-                        onChange={this.props.changed} 
-                        value={this.props.name}
-                    />
-                {/* </div> */}
-            </Aux>
+            
+            <React.Fragment>
+                <p onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old.
+                </p>
+                <p>{this.props.children}</p>
+                <input 
+                    type='text' 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                />
+            </React.Fragment>
+            
         );
     }
 }
