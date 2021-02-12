7915ca4f1e1279b55271d4f039347bf6d426ba07
Manasi Keskar <ManasiKeskar@users.noreply.github.com>
2021-02-12 13:59:38 -0800

    useRef() hook for functional component like cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.module.css';
 
 const Cockpit = ( props ) => {
 
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
       console.log('[Cockpit.js] useEffect');
-      setTimeout(() => {
-        alert('Saved data to cloud');
-      }, 1000);
+      // setTimeout(() => {
+      //   alert('Saved data to cloud');
+      // }, 1000);
+      toggleBtnRef.current.click();
       return() => {
         console.log('[Cockpit,js] cleanup work in useEffect');
       };
@@ -40,6 +43,7 @@ const Cockpit = ( props ) => {
             <p className={assignedClasses.join(' ')}>This is really working!!</p>
             {/* <button onClick={this.switchNameHandler.bind(this, 'Maxmilian')}>Switch Name</button> */}
             <button
+              ref={toggleBtnRef}
               className={btnClass}
               onClick={props.clicked}
             >
